18a7d8a64b2205a1ada8fbbf4269cb208fa49ad5
Franco Benitez <franco.benitez@est.fi.uncoma.edu.ar>
2024-06-05 11:08:10 -0300

    Cambios en seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PostsTableSeeder::class);
+        $this->call(PostsTableSeeder::class,
+        UsersTableSeeder::class
+    );
     }
 }
