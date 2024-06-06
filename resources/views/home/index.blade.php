9b078b9bfd32204eed32b14b9359478dc32355fc
Santiag0Vidal <david.vidal@est.fi.uncoma.edu.ar>
2024-06-06 11:44:04 -0300

    puse el buscador en home y remplase el contenido del post que se mustra en las card por el autor y otras cositas

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,14 +5,25 @@
 @endsection
 
 @section('content')
-    <h1>Home</h1>
+    <div class="max-w-2xl mx-auto p-6 bg-white rounded">
+    <h1 class="text-3xl font-bold text-gray-800 mb-6 text-center">Posts</h1>
+
+    <form action="{{ route('posts.search') }}" method="GET" class="mb-6">
+        <div class="flex">
+            <input type="text" name="query" class="w-full p-2 border border-gray-300 rounded-l focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Buscar posts...">
+            <button type="submit" class="bg-blue-600 text-white py-2 px-4 rounded-r hover:bg-blue-700 transition">Buscar</button>
+        </div>
+    </form>
+    </div>
+
+    
     <div class="flex flex-wrap -m-4">
         @foreach ($posts as $post)
             <a href="{{ route('post.show', $post->id) }}" class="card">
                 <img class="card-image" src="/images/crepe.jpg" alt="">
                 <div class="card-content">
-                    <h5 class="card-title truncate overflow-hidden overflow-ellipsis">{{ $post->title }}</h5>
-                    <p class="card-text truncate overflow-hidden overflow-ellipsis">{{ Str::limit($post->content, 100) }}</p>
+                    <h5 class="card-title truncate overflow-hidden overflow-ellipsis">{{ Str::limit($post->title, 25) }}</h5>
+                    <p class="card-text truncate overflow-hidden overflow-ellipsis">Autor: {{ Str::limit($post->poster, 100) }}</p>
                 </div>
             </a>
         @endforeach
