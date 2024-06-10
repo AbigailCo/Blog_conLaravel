a884142fefbcb48024d2e107944e0130d7bac895
melaniquininires <melaniquininires@gmail.com>
2024-06-10 17:15:19 -0300

    borre el add likes to post para que no tire error

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,11 +26,11 @@ class Post extends Model
     }
     public function category()
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class); 
     }
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class); 
     }
     
 }
